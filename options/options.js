2b17c01ca506b3f9ce1dcf805be91abf78c2b434
isaac-newt <isaac.newt.list@icloud.com>
2017-11-14 21:18:42 -0600

    works

--- options/options.js
+++ options/options.js
@@ -9,20 +9,25 @@ function saveOptions(e) {
   // image: document.querySelector("#image").value
 }
 
+function displayImageSettings() {
+  // Hide image-specific settings unless needed
+  if (document.querySelector("#theme").value != "Image") {
+    var div0 = document.getElementById("image-locate");
+    div0.style.display = "none";
+    var div1 = document.getElementById("local-select");
+    div1.style.display = "none";
+  }
+}
+
 function restoreOptions() {
 
   function setCurrentChoice(result) {
     document.querySelector("#color").value = result.color || "";
     document.querySelector("#theme").value = result.theme || "Default";
     // document.querySelector("#theme").value = result.image || "";
-    
+
     // Hide image-specific settings unless needed
-    if (document.querySelector("#theme").value != "Image") {
-      var div0 = document.getElementById("image-locate");
-      div0.style.display = "none";
-      var div1 = document.getElementById("local-select");
-      div1.style.display = "none";
-    }
+    displayImageSettings();
   }
 
   function onError(error) {
@@ -37,9 +42,10 @@ function showImageOptions() {
   console.log("Yay!");
 }
 
+//run showImageOptions when checkbox checked
+document.getElementById("image-location").addEventListener("change", showImageOptions);
+
 //runs restoreOptions when page is loaded
 document.addEventListener("DOMContentLoaded", restoreOptions);
 //runs saveOptions when submit button is clicked
 document.querySelector("form").addEventListener("submit", saveOptions);
-//run showImageOptions when checkbox checked
-document.getElementById("image-location").addEventListener("checked", showImageOptions);
