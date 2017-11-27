cd9dbdf4b5a019df4b31f01d9687b0b770ff1aa8
isaac-newt <isaac.newt.list@icloud.com>
2017-11-26 22:19:04 -0600

    the skeleton is done, now for functionality

--- options/options.js
+++ options/options.js
@@ -5,45 +5,29 @@ function saveOptions(e) {
   browser.storage.local.set({
     color: document.querySelector("#color").value,
     theme: document.querySelector("#theme").value,
+    image: document.querySelector("#image").value
   });
-  // image: document.querySelector("#image").value
 }
 
 function displayImageSettings() {
   // Hide image-specific settings unless needed
+  var div0 = document.getElementById("local-select");
   if (document.querySelector("#theme").value == "Image") {
-    var div0 = document.getElementById("image-locate");
     div0.style.display = "block";
-    var div1 = document.getElementById("local-select");
-    div1.style.display = "block";
   } else if (document.querySelector("#theme").value != "Image") {
-    var div0 = document.getElementById("image-locate");
     div0.style.display = "none";
-    var div1 = document.getElementById("local-select");
-    div1.style.display = "none";
   }
 }
 
 // Show image-specific settings when "image" selected
 document.getElementById("theme").addEventListener("change", displayImageSettings);
 
-function displayBackgroundPicker() {
-  if (document.querySelector("#image-location").checked) {
-    console.log("show");
-  } else {
-    console.log("hide");
-  }
-}
-
-// Show background picker when checkbox is checked
-document.getElementById("image-location").addEventListener("change", displayBackgroundPicker);
-
 function restoreOptions() {
 
   function setCurrentChoice(result) {
     document.querySelector("#color").value = result.color || "";
     document.querySelector("#theme").value = result.theme || "Default";
-    // document.querySelector("#theme").value = result.image || "";
+    document.querySelector("#image").value = result.image || "";
 
     // Hide image-specific settings unless needed
     displayImageSettings();
