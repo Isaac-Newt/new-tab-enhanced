6808b95610cdb3352c24636e2e85834480ddef07
isaac-newt <isaac.newt.list@icloud.com>
2017-12-16 22:40:07 -0600

    Some more updates to stuff.

--- options/options.js
+++ options/options.js
@@ -5,8 +5,10 @@ function saveOptions(e) {
   browser.storage.local.set({
     color: document.querySelector("#color").value,
     theme: document.querySelector("#theme").value,
-    image: document.querySelector("#image").value
+    image: document.querySelector("#image").value,
+    weather: document.querySelector("#weather").checked
   });
+  console.log(document.querySelector('#weather').checked);
 }
 
 function displayImageSettings() {
@@ -28,7 +30,14 @@ function restoreOptions() {
     document.querySelector("#color").value = result.color || "";
     document.querySelector("#theme").value = result.theme || "Default";
     document.querySelector("#image").value = result.image || "";
-
+    /*
+     * Need to figure out how to set default value for checkboxes.
+     * Doing "...|| true;" always returns true, and seems to
+     * override the saved value.  It's weird.
+     */
+    document.querySelector("#weather").checked = result.weather;
+    // Check value, remove for production
+    console.log(document.querySelector('#weather').checked);
     // Hide image-specific settings unless needed
     displayImageSettings();
   }
@@ -37,11 +46,18 @@ function restoreOptions() {
     console.log(`Error: ${error}`);
   }
 
-  var getting = browser.storage.local.get(["color", "theme"]);
+  var getting = browser.storage.local.get(["color", "theme", "weather"]);
   getting.then(setCurrentChoice, onError);
+}
+
+// Troubleshooting, remove for production!!!
+function consoleHelp() {
+  console.log(document.querySelector("#weather").checked);
 }
 
 //runs restoreOptions when page is loaded
 document.addEventListener("DOMContentLoaded", restoreOptions);
 //runs saveOptions when submit button is clicked
 document.querySelector("form").addEventListener("submit", saveOptions);
+// Remove in production!!!
+document.querySelector("#weather").addEventListener("change", consoleHelp);
